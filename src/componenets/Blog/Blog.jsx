b97c857c5471230d7d7a9c4eb3b33526e9af954e
ataur28 <ataurrahman862828@gmail.com>
2023-04-01 04:51:56 +0600

    display some blog information

--- src/componenets/Blog/Blog.jsx
+++ src/componenets/Blog/Blog.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import './Blog.css'
 
 const Blog = (props) => {
-    console.log(props.blog);
-    const {authorName, authorPicture, blogTitle, coverPicture, publishDate, readTime
+    // console.log(props.blog);
+    const { authorName, authorPicture, blogTitle, coverPicture, publishDate, readTime
     } = props.blog;
-    
+
     return (
-        <div>
-           <h3>this is blog</h3> 
+        <div className='blog-container'>
+            <img className='cover-image' src={coverPicture} alt="" />
+            <div className='blog-author-container'>
+                <div className='author-container'>
+                    <img className='author-image' src={authorPicture} alt="" />
+                    <div>
+                        <h4 className='author-name'>{authorName}</h4>
+                        <p className='publishing-date'>{publishDate}</p>
+                    </div>
+                </div>
+                <div className='read-time'>
+                    <p>{readTime} min read</p>
+
+                </div>
+            </div>
         </div>
     );
 };
